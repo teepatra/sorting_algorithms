a7f020852754b8534aa80c46b238eea4f1b754e3
teepatra <fagboretomilola@gmail.com>
2023-07-25 15:36:44 +0000

    sorts an array of integers in ascending order using the Shell sort algorithm

---
+++ 100-shell_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+void insertion_sort(int *, long int, long int, size_t);
+
+/**
+* shell_sort - shell sort algorithm
+*
+* @array: array to sort
+* @size: size of array
+*/
+void shell_sort(int *array, size_t size)
+{
+	long int n = 1;
+	long int i, k, tmp;
+
+	if (size < 2)
+		return;
+
+	while (n < (long int)size / 3)
+		n = n * 3 + 1;
+
+	while (n > 0)
+	{
+		for (i = n; i < (long int) size; i++)
+		{
+			tmp = array[i];
+			for (k = i; k >= n && array[k - n] > tmp; k = k - n)
+				array[k] = array[k - n];
+			array[k] = tmp;
+		}
+		print_array(array, size);
+		n = (n - 1) / 3;
+	}
+}
